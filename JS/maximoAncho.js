52e2ac38081fcc51ead5336bd4645c584ab9401d
SamJunior <81992471+raxi6597@users.noreply.github.com>
2024-03-05 15:07:34 -0600

    Delete JS/maximoAncho.js

--- JS/maximoAncho.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-  var width = window.innerWidth;
-
-  // Redirigir a otra página si el ancho de la ventana es mayor que 769px
-  if (width > 769) {
-    window.location.href = 'Error.html';
-  }
-}
